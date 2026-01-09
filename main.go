a5d58a11c03242d31161a94687a147be8eb95146
OmarMohammedElsheikh <omarmohammedomarali@gmail.com>
2026-01-09 16:01:32 +0200

    implementing validation and GET by id

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
         "net/http"
 
         "github.com/gin-gonic/gin"
+
+        "strconv"
+
 )
 
 type album struct {
@@ -23,6 +26,7 @@ var albums = []album{
 func main() {
 	r := gin.Default()
 	r.GET("/albums", get_alb)
+	r.GET("/albums/:id",getalbID)
 	r.POST("/albums", post_alb)
 	r.Run("localhost:8080")
 }
@@ -35,10 +39,34 @@ func get_alb(a *gin.Context) {
 func post_alb(a *gin.Context) {
 	var newAlbum album 
 	err := a.ShouldBindJSON(&newAlbum)
+
 	if err != nil {
+		a.IndentedJSON(400,gin.H{"message": `you should follow this structure {
+			title : album_title,
+			artist : album_artist,
+			price : non-negative price 
+		}`})
+		return
+	}
+	if newAlbum.Price <= 0 || newAlbum.Price > 1000 {
+		a.IndentedJSON(400, gin.H{"message": "please enter a valid price"})
 		return
 	}
 
+	newAlbum.ID = strconv.Itoa(len(albums) + 1)
+	
 	albums = append(albums, newAlbum)
 	a.IndentedJSON(http.StatusCreated,newAlbum)
+}
+
+
+func getalbID(a *gin.Context) {
+	id := a.Param("id")
+	for _, al := range albums {
+		if al.ID == id {
+			a.IndentedJSON(http.StatusOK,al)
+			return
+		}
+	}
+	a.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found!"})
 }
